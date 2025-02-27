65efbf704f37465f87ab4a02fba379ead943b190
abdullah2310ishaq <abdullahishaq2310@gmail.com>
2025-02-28 04:07:37 +0500

    update

--- src/app/types/Product.ts
+++ src/app/types/Product.ts
@@ -4,9 +4,3 @@ export type Product = {
     price: string;
     img: string;
   };
-export type Product = {
-  id: string; 
-  name: string;
-  price: number; // Changed to number for accurate price representation
-  img: string;
-};  
