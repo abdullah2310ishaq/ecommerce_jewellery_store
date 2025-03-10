e6ba5d874c8c128e8b043da2b959b033e9d2e033
abdullah2310ishaq <abdullahishaq2310@gmail.com>
2025-03-10 14:24:51 +0500

    fix:config

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,15 @@ const nextConfig: NextConfig = {
     domains: [
       "images.pexels.com",
       "firebasestorage.googleapis.com",
-      "lh3.googleusercontent.com",  // Google Photos & Drive Images
-      "drive.google.com", // Google Drive (May still need URL handling)
+      "lh3.googleusercontent.com", // Google Photos
+      "drive.google.com", // Google Drive (May not work directly)
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+        pathname: "/uc**", // Allow Google Drive images
+      },
     ],
   },
 };
